560498082d79bc4d1d62bdb667a5a1a9103eccf4
smallville <abutimartin778@gmail.com>
2025-05-16 16:46:18 +0300

    c linux

--- src/app/dashboard/team/page.tsx
+++ src/app/dashboard/team/page.tsx
@@ -1,8 +1,14 @@
-"use client"
 import Dashboard from "@/ui/components/dash/Dashboard";
 import TeamManagement from "@/app/dashboard/team/page.view";
+import Accounts from "@/lib/accounts";
+import {UserRole} from "@/models";
+import {notFound} from "next/navigation";
 
-export default function Page() {
+export default async function Page() {
+    const user = await Accounts.user();
+    if (!user || user.role !== UserRole.ADMIN) {
+        return notFound();
+    }
     return (
         <>
             <Dashboard>
