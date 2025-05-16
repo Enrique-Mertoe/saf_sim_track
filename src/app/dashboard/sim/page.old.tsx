37852b1ce27bc21571c3ce0aa886d9dd512e7a9b
Wilson-kali <wilsonndambuki47@gmail.com>
2025-05-16 07:38:09 +0300

    c1

--- src/app/dashboard/sim/page.old.tsx
+++ src/app/dashboard/sim/page.old.tsx
@@ -1,10 +1,10 @@
-import Dashboard from "@/ui/components/dash/Dashboard";
-import SIMRegistrationPage from "@/app/dashboard/sim/page.view";
-
-export default function Page() {
-    return (
-        <Dashboard>
-            <SIMRegistrationPage/>
-        </Dashboard>
-    )
-}
+// import Dashboard from "@/ui/components/dash/Dashboard";
+// import SIMRegistrationPage from "@/app/dashboard/sim/page.view";
+//
+// export default function Page() {
+//     return (
+//         <Dashboard>
+//             <SIMRegistrationPage/>
+//         </Dashboard>
+//     )
+// }
