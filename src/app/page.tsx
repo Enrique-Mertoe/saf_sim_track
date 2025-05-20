38f9f14d83e8a62974f5e72c362587572f1978b4
smallville <abutimartin778@gmail.com>
2025-05-20 12:39:32 +0300

    c linux

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
 import Home from "@/app/HomePage";
+import Accounts from "@/lib/accounts";
+import {redirect} from "next/navigation";
 export default async function Home1() {
-
+const user = await Accounts.user();
+    if (user) {
+        return redirect("/accounts/dashboard")
+    }
     return (
         <Home/>
     );
