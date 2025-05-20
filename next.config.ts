5f56d116d3430fc29e6d2bfeb3826a31ef535b52
smallville <abutimartin778@gmail.com>
2025-05-20 12:38:26 +0300

    c linux

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,6 @@
 import type {NextConfig} from "next";
 
 const nextConfig: NextConfig = {
-    /* config options here */
-    webpack: (config) => {
-        // This is to handle the worker files in PDF.js
-        config.resolve.alias['pdfjs-dist'] = require.resolve('pdfjs-dist');
-        return config;
-    },
 };
 
 export default nextConfig;
