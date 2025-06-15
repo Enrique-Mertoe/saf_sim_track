491505467b532e7bb00ba8f13632aaf47553544b
mca <abutimartin778@gmail.com>
2025-06-15 14:58:42 +0300

    c2

--- src/app/dashboard/sim/page.tsx
+++ src/app/dashboard/sim/page.tsx
@@ -1,12 +1,10 @@
 import Accounts from "@/lib/accounts";
-import {notFound} from "next/navigation";
-import {UserRole} from "@/models";
 import StaffUploadPage from "@/app/dashboard/sim/page.view";
 
 export default async function Page() {
     const user = await Accounts.user();
-    if (!user || user.role !== UserRole.STAFF) {
-        return notFound();
-    }
+    // if (!user || user.role !== UserRole.STAFF) {
+    //     return notFound();
+    // }
     return <StaffUploadPage/>
 }
