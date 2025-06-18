4ac47ba654324f572c01ac1053502e45ce728fc0
mca <abutimartin778@gmail.com>
2025-06-18 10:26:47 +0300

    c2

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -23,6 +23,7 @@ const eslintConfig = [
             }],
             "@typescript-eslint/no-explicit-any": "off",
             "prefer-const": "off",
+            "@typescript-eslint/no-empty-object-type":"off",
             "@next/next/no-html-link-for-pages": "off",
             "@typescript-eslint/no-require-imports":"off",
             "react/no-unescaped-entities": "off",
